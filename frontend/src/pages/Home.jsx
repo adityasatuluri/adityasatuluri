138bce858e6bc79a8e8001010efd0c57c4615bfe
aditya.satuluri <s.aditya.in@gmail.com>
2025-10-20 20:00:16 +0530

    fix day night cycle section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -14,7 +14,7 @@ import BlurText from "../components/Blurtext.jsx";
 
 const WhiteBg = new URL("../assets/WhiteBg.webp", import.meta.url).href;
 const WhiteBgM = new URL("../assets/WhiteBgM.webp", import.meta.url).href;
-const morning = new URL("../assets/cy-city-morning.webp", import.meta.url).href;
+import morning from "../assets/cy-city-morning.webp";
 const night = new URL("../assets/cy-city.webp", import.meta.url).href;
 const item2077 = new URL("../assets/item2077.webp", import.meta.url).href;
 
